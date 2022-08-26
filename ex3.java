3d20c06806c4d9f2e59ec7796b72243b6178ea76
André Bigaran <84354648+Andre-Bigaran@users.noreply.github.com>
2022-08-26 01:00:58 -0300

    Update ex3.java

--- ex3.java
+++ ex3.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.prova;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class ex3 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double massa = 0, massaI;
+        double tempo = 50, segundos = 0, minutos = 0, horas = 0;
+        int i = 0;
+
+        System.out.println("Informe a massa: ");
+        massa = sc.nextDouble();
+        massaI = massa;
+
+        while (massa > 0.5) {
+            massa = massa / 2;
+            i++;
+        }
+
+        tempo = tempo * i;
+        segundos = tempo;
+        minutos = tempo / 60;
+        horas = minutos / 60;
+
+        System.out.println("Massa inicial: " + massaI);
+        System.out.println("Massa final: " + massa);
+        System.out.println("Horas: " + horas);
+        System.out.println("Minutos: " + minutos);
+        System.out.println("Segundos: " + segundos);
+
+    }
+
 }
