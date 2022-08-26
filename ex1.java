bfeb49a267b2894b73a166825024d7a341c3587b
Andre-Bigaran <andrevbigaran12@gmail.com>
2022-08-26 00:59:07 -0300

    add

---
+++ ex1.java
@@ -0,0 +1,36 @@
+package com.mycompany.prova;
+
+import java.util.Scanner;
+
+public class ex1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numb, maior = 0, menor = 0;
+        int x;
+
+        System.out.println("Insira 5 valores inteiros: ");
+
+        for (x = 0; x < 5; x++) {
+
+            System.out.print((x + 1) + "- Insira numero: ");
+            numb = sc.nextInt();
+
+            if (x == 0) {
+                maior = numb;
+                menor = numb;
+            }
+            if (numb > maior) {
+                maior = numb;
+            }
+            if (numb < menor) {
+                menor = numb;
+            }
+        }
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+
+    }
+}
