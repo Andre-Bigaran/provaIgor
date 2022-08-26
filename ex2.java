22351f1a47b76ee904a1ffaddfddfbda5610a371
André Bigaran <84354648+Andre-Bigaran@users.noreply.github.com>
2022-08-26 01:01:19 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.prova;
 
-/**
- *
- * @author Usuario
- */
 public class ex2 {
-    
+
+    public static void main(String[] args) {
+
+        double percentual = 0;
+        double salario = 1000;
+        int x, i = 0;
+
+        percentual = (1.5 / 100);
+        salario = salario + (salario * percentual);
+
+        for (x = 1997; x < 2022; x++) {
+            percentual = percentual * 2;
+            salario = salario + (salario * percentual);
+        }
+
+        System.out.println("Salario atual de " + x + ": R$" + salario);
+    }
 }
